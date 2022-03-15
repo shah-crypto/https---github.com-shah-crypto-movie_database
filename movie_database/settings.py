6c0a74ca449109fc4bbd99f832cd2f16adb300b4
Pratham Shah <prathamshah2507@gmail.com>
2022-03-15 19:18:57 +0530

    Add Procfile and update settings

--- movie_database/settings.py
+++ movie_database/settings.py
@@ -24,9 +24,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 't_*u!^wzqlvf-(bu3_&#$&a*r2@rh=g+v-2e8sa%w6pi5o_v!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
